1b4b64d019a41cd2ecb0a1a9623b1d19200ac46a
xs <xingshenwork@163.com>
2019-11-14 14:21:28 +0800

    spring boot 测试演示

--- spring-boot-2.0-samples/first-app-by-gui/src/test/java/thinking/in/spring/boot/firstappbygui/FirstAppByGuiApplicationTests.java
+++ spring-boot-2.0-samples/first-app-by-gui/src/test/java/thinking/in/spring/boot/firstappbygui/FirstAppByGuiApplicationTests.java
@@ -2,15 +2,20 @@ package thinking.in.spring.boot.firstappbygui;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import thinking.in.spring.boot.config.WebConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = WebConfiguration.class)
 public class FirstAppByGuiApplicationTests {
-
+	@Autowired
+	private ApplicationRunner applicationRunner;
 	@Test
 	public void contextLoads() {
+		System.out.println();
 	}
 
 }
