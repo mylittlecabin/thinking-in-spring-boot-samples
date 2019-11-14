3ca581088ac7a6cb760575307de0651b50d9f0a6
xs <xingshenwork@163.com>
2019-11-14 14:18:08 +0800

    关于spring boot引导类。它位置并不局限于我们常见的groupId路径下,可以随意放置。 1.main方法定位应用入口； 2.注解用于触发自动装配；

---
+++ spring-boot-2.0-samples/first-app-by-gui/src/main/java/thinking/in/spring/boot/firstappbygui/fg/FG.java
@@ -0,0 +1,19 @@
+package thinking.in.spring.boot.firstappbygui.fg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * @author: xs
+ * @date: 19-11-14 13:58:56
+ * @description: 验证spring boot怎样查找引导类的；
+ *               要求有注解（一般是@SpringBootAppliction,@EnableAutoConfiguration也可）；有唯一main方法；
+ *               注解用于自动装配，main方法定位应用入口；
+ */
+//@EnableAutoConfiguration
+public class FG {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(FG.class, args);
+//    }
+}
